b9db2332343b432af5cf952619e7f849e6e17ce4
Varokas Panusuwan <varokas@panusuwan.com>
2016-12-31 19:04:05 +0700

    add drivers

--- src/main/scala/executor/Executor.scala
+++ src/main/scala/executor/Executor.scala
@@ -1,6 +1,7 @@
 package executor
 
 import com.typesafe.scalalogging.LazyLogging
+import org.openqa.selenium.firefox.FirefoxDriver
 import story.Story
 
 class Executor extends LazyLogging {
@@ -8,13 +9,24 @@ class Executor extends LazyLogging {
 
     logger.debug("Executing story: " + story)
 
+    val startTime = System.currentTimeMillis
+
     val executeContext = new ExecuteContext()
-    for(step <- story.steps)
-      step.doStep(executeContext)
+    for(step <- story.steps) {
+
+
+      try {
+        step.doStep(executeContext)
+      } finally {
+
+      }
+    }
   }
 }
-
+class StepResult(
+  var time: Option[Long]
+)
 
 class ExecuteContext {
-
+  val driver = new FirefoxDriver();
 }
