08bc627c44d364deb1ddac5ae5fcaecfd408959f
Varokas Panusuwan <varokas@panusuwan.com>
2016-12-31 18:48:31 +0700

    add executor

--- src/main/scala/executor/Executor.scala
+++ src/main/scala/executor/Executor.scala
@@ -1,9 +1,13 @@
 package executor
 
+import com.typesafe.scalalogging.LazyLogging
 import story.Story
 
-class Executor {
-  def execute(story: Story): Unit = {
+class Executor extends LazyLogging {
+  def execute(runId: String, story: Story): Unit = {
+
+    logger.debug("Executing story: " + story)
+
     val executeContext = new ExecuteContext()
     for(step <- story.steps)
       step.doStep(executeContext)
