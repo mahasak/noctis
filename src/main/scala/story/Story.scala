d1d84afddc24ef4b9332be8b8be71affc938ee51
Mahasak Pijittum <mahasak@gmail.com>
2016-12-31 20:39:51 +0700

    refactor

--- src/main/scala/story/Story.scala
+++ src/main/scala/story/Story.scala
@@ -6,44 +6,44 @@ import story.StepResultStatus.StepResultStatus
 case class Story(steps: Seq[Step])
 
 trait Step {
-  def doStep(executeContext: ExecuteContext) : StepResult
+  def doStep(executeContext: ExecuteContext): StepResult
 }
 
 trait DoStep extends Step
+
 trait AssertStep extends Step
 
 case class StepGoto(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    return StepResult.Pass
+    StepResult.Pass
   }
 }
 
 case class StepClick(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    return StepResult.Pass
+    StepResult.Pass
   }
 }
 
 case class StepType(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    return StepResult.Pass
+    StepResult.Pass
   }
 }
 
 case class StepAssert(param: StepParameter) extends AssertStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    return StepResult.Pass
+    StepResult.Pass
   }
 }
 
 case class StepNoop(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    return StepResult.Pass
+    StepResult.Pass
   }
 }
 
 class UrsaParser {
-
   def stepMatcher(string: String): Step = {
     val regexGoto = """Go to ([a-z]+)""".r
     val regexClick = """(Click).*([^\s]+)""".r
@@ -51,25 +51,27 @@ class UrsaParser {
     val regexAssert = """(Assert).*([^\s]+)""".r
 
     string match {
-      case regexGoto(url) => new StepGoto(new StepParameter("", url, ""))
-      case regexClick(_*) => new StepClick(new StepParameter)
-      case regexType(_*) => new StepType(new StepParameter)
-      case regexAssert(_*) => new StepAssert(new StepParameter)
-      case _ => new StepNoop(new StepParameter)
+      case regexGoto(url) => StepGoto(StepParameter("", url, ""))
+      case regexClick(_*) => StepClick(StepParameter())
+      case regexType(_*) => StepType(StepParameter())
+      case regexAssert(_*) => StepAssert(StepParameter())
+      case _ => StepNoop(new StepParameter)
     }
   }
 
-  def parseStory(str: String): Story = new Story(str.split("\n")
+  def parseStory(str: String) = Story(str.split("\n")
     .map(_.trim)
     .filter(s => !s.isEmpty)
     .map(s => stepMatcher(s)))
 
 }
 
-
-case class StepParameter(id: String = "", url: String ="", expect: String= "")
+case class StepParameter(id: String = ""
+                         , url: String = ""
+                         , expect: String = "")
 
 case class StepResult(status: StepResultStatus)
+
 object StepResult {
   val Pass = StepResult(StepResultStatus.Pass)
   val Fail = StepResult(StepResultStatus.Fail)
@@ -80,4 +82,3 @@ object StepResultStatus extends Enumeration {
   val Pass = Value(0)
   val Fail = Value(1)
 }
-
