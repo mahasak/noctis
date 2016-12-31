0c8bfd0f38010207c192d72615e3c3f6823ddddc
Mahasak Pijittum <mahasak@gmail.com>
2016-12-31 15:36:37 +0700

    Add jbehave story, step and user story

---
+++ src/test/scala/com/bigbears/jbehave/story/ScalaStories.java
@@ -0,0 +1,40 @@
+package com.bigbears.jbehave.story;
+
+/**
+ * Created by mahasak on 12/31/2016 AD.
+ */
+
+import java.util.List;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.configuration.scala.ScalaContext;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.scala.ScalaStepsFactory;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.ANSI_CONSOLE;
+
+public class ScalaStories extends JUnitStories {
+
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration()
+                .useStoryReporterBuilder(new StoryReporterBuilder().withFormats(ANSI_CONSOLE));
+    }
+
+    @Override
+    protected List<String> storyPaths() {
+        return new StoryFinder()
+                .findPaths(codeLocationFromClass(this.getClass()), "**/*.story", "");
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new ScalaStepsFactory(configuration(), new ScalaContext("ScalaSteps"));
+    }
+
+}
