6f10165dec73f9c21992cf05138db1cdd8921bfa
Mahasak Pijittum <mahasak@gmail.com>
2017-01-07 13:20:15 +0700

    Add more step and text assert step

--- src/main/scala/story/Story.scala
+++ src/main/scala/story/Story.scala
@@ -15,6 +15,11 @@ trait Step {
       })
   }
 
+  def getElementById(driver: WebDriver, id: String) : WebElement = {
+      waitFor(driver, _.findElement(By.id(id)), 5)
+      return driver.findElement(By.id(id))
+  }
+
   def doStep(executeContext: ExecuteContext): StepResult
 }
 
@@ -38,26 +43,35 @@ case class StepClose(param: StepParameter) extends DoStep {
 
 case class StepClick(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    waitFor(executeContext.driver, _.findElement(By.id(param.id)), 5)
-    executeContext.driver.findElement(By.id(param.id)).click()
+    getElementById(executeContext.driver, param.id).click
+    StepResult.Pass
+  }
+}
+
+case class StepSubmit(param: StepParameter) extends DoStep {
+  def doStep(executeContext: ExecuteContext): StepResult = {
+    getElementById(executeContext.driver, param.id).submit
+    StepResult.Pass
+  }
+}
+
+case class StepClear(param: StepParameter) extends DoStep {
+  def doStep(executeContext: ExecuteContext): StepResult = {
+    getElementById(executeContext.driver, param.id).clear
     StepResult.Pass
   }
 }
 
 case class StepType(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    waitFor(executeContext.driver, _.findElement(By.id(param.id)), 5)
-    val e = executeContext.driver.findElement(By.id(param.id))
-    e.clear
-    e.sendKeys(param.expect)
+    getElementById(executeContext.driver, param.id).sendKeys(param.expect)
     StepResult.Pass
   }
 }
 
 case class StepPressEnter(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
-    waitFor(executeContext.driver, _.findElement(By.id(param.id)), 5)
-    executeContext.driver.findElement(By.id(param.id)).sendKeys(Keys.RETURN)
+    getElementById(executeContext.driver, param.id).sendKeys(Keys.RETURN)
     StepResult.Pass
   }
 }
@@ -74,6 +88,30 @@ case class StepAssert(param: StepParameter) extends AssertStep {
   }
 }
 
+case class StepShouldDisplayText(param: StepParameter) extends AssertStep {
+  def doStep(executeContext: ExecuteContext): StepResult = {
+    println(getElementById(executeContext.driver, param.id).getAttribute("value"))
+    getElementById(executeContext.driver, param.id)
+      .getAttribute("value")
+      .equals(param.expect) match {
+      case true => StepResult.Pass
+      case _ => StepResult.Fail
+    }
+  }
+}
+
+case class StepShouldNotDisplayText(param: StepParameter) extends AssertStep {
+  def doStep(executeContext: ExecuteContext): StepResult = {
+    println(getElementById(executeContext.driver, param.id).getAttribute("value"))
+    getElementById(executeContext.driver, param.id)
+      .getAttribute("value")
+      .equals(param.expect) match {
+      case false => StepResult.Pass
+      case _ => StepResult.Fail
+    }
+  }
+}
+
 case class StepNoop(param: StepParameter) extends DoStep {
   def doStep(executeContext: ExecuteContext): StepResult = {
     StepResult.Pass
@@ -85,19 +123,33 @@ class UrsaParser {
     val regexGoto = """(Go to) "(.*?)"""".r
     val regexClick = """Click on "(.*?)"""".r
     val regexType = """Type "(.*?)" in "(.*?)"""".r
+    val regexClear = """Clear "(.*?)"""".r
     val regexPressEnter = """Press ENTER on "(.*?)"""".r
     val regexAssert = """(Assert).*([^\s]+)""".r
+    val regexDisplay = """It should (.*?) display "(.*?)" in "(.*?)"""".r
     val regexWaitFor = """Wait for "(.*?)" ms""".r
+    val regexSubmit = """Submit "(.*?)"""".r
     val regexClose = """(Close browser)""".r
 
     string match {
       case regexGoto(_, url) => StepGoto(StepParameter("", url, ""))
       case regexClick(id) => StepClick(StepParameter(id,"",""))
       case regexType(text,id) => StepType(StepParameter(id, "", text))
+      case regexClear(id) => StepClear(StepParameter(id, "", ""))
       case regexPressEnter(id) => StepPressEnter(StepParameter(id, "", ""))
       case regexAssert(_*) => StepAssert(StepParameter())
+      case regexDisplay(condition,text,id) => displayAssertMatcher(condition,id,text)
       case regexClose(_*) => StepClose(StepParameter())
       case regexWaitFor(ms) => StepWait(StepParameter("", "", ms))
+      case regexSubmit(id) => StepSubmit(StepParameter(id,"",""))
+      case _ => StepNoop(new StepParameter)
+    }
+  }
+
+  def displayAssertMatcher(condition: String, id:String, text: String): Step = {
+    condition match {
+      case "be" => StepShouldDisplayText(StepParameter(id, "", text))
+      case "not" => StepShouldNotDisplayText(StepParameter(id, "", text))
       case _ => StepNoop(new StepParameter)
     }
   }
