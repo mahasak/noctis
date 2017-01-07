c0dfaafdcf9616cf111bf326369c33c397dbf83a
Varokas Panusuwan <varokas@panusuwan.com>
2017-01-07 13:27:47 +0700

    copy sample executor to main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import com.typesafe.scalalogging.LazyLogging
 import db.SchemaMigration
+import executor.{ExecuteContext, Executor}
 import story.UrsaParser
 
 object Main extends LazyLogging {
@@ -8,5 +9,27 @@ object Main extends LazyLogging {
     logger.info("Running liquibase")
 
     SchemaMigration.run()
+
+    val parser = new UrsaParser
+    val storyTest =
+      """
+        |Go to "http://www.google.com"
+        |Type "BigBears.IO" in "lst-ib"
+        |Click on "_fZl"
+        |Wait for "2000" ms
+        |Type "Norbor" in "lst-ib"
+        |Click on "_fZl"
+        |Wait for "2000" ms
+        |Type "Varokas Panusuwan" in "lst-ib"
+        |Click on "_fZl"
+        |Wait for "2000" ms
+        |Close browser
+      """.stripMargin
+
+    val executor = new Executor()
+    val executeContext = new ExecuteContext("test-002")
+    val story = parser.parseStory(storyTest)
+    val result = executor.execute("test-002", story)
+    println(result)
   }
 }
